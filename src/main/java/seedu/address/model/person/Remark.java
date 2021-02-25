4bed096d2fb3091a883c6d0130bca8ff22b908c5
Zeche <zx3110@gmail.com>
2021-02-25 17:44:34 +0800

    Add missing javadocs

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates a Remark object to be associated with a Person object
+     * @param remark a non null remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
